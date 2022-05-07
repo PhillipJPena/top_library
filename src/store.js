37a3417bf71fbac8e33713c3ca71ee9c21f8cdc9
Phillip Pena <ppena941@gmail.com>
2022-05-06 22:02:19 -0400

    added local storage feature

--- src/store.js
+++ src/store.js
@@ -2,11 +2,14 @@
 import { writable } from "svelte/store";
 import Book from "./models/bookModel";
 
-export const books = writable([
-  new Book({title:'Book 1', author:"Doctor Seuss", pages:23}),
-  new Book({title:'Book 2', author:"Doctor Seuss", pages:23}),
-  new Book({title:'Book 3', author:"Doctor Seuss", pages:23}),
+const stored = localStorage.content
+
+export const books = writable(JSON.parse(localStorage.getItem('book')) || [
+  new Book ({title:"Little Red Riding Hood", author:"Aesop", pages:12, isRead:true})
 ]);
 
+books.subscribe((content) => localStorage.book = JSON.stringify(content))
+
 export const modalState = writable(false);
+
 export const formType = writable(null);
